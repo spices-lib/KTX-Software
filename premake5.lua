b29785ef27b4b73a5964a30dbcc5d4ff74d263db
Spiecs <spiecs@outlook.com>
2025-07-13 12:16:34 +0800

    using c++20

--- premake5.lua
+++ premake5.lua
@@ -82,13 +82,13 @@ project "ktx"
 
 	filter "system:windows"
 		systemversion "latest"
-		cppdialect "C++17"
+		cppdialect "C++20"
 		staticruntime "On"
 
 	filter "system:linux"
 		pic "On"
 		systemversion "latest"
-		cppdialect "C++17"
+		cppdialect "C++20"
 		staticruntime "On"
 
 	filter "configurations:Debug"
